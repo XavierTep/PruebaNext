fcd01161762fee93d207a9e3959fad7ce711fdcd
XavierTracom <xtepud@tracom.es>
2024-06-10 09:52:27 +0200

    Prueba proyecto 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['clientes.tracom.info', 'gestion.tracom.info'], 
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/pdf/:path*', 
+          destination: 'https://clientes.tracom.info/:path*', 
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
